b8c81c3d179d27f56167bf4b993070a688bfad63
urbanskimichall <urbmich.2000@gmail.com>
2026-01-10 19:19:09 +0100

    Fix crash on text drawing

--- src/ProgressBar.hpp
+++ src/ProgressBar.hpp
@@ -1,19 +1,24 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 #include <algorithm>
+#include "resource/FontManager.hpp"
 
 class ProgressBar
 {
 public:
     ProgressBar(float width, float height)
         : background({width, height}),
-          foreground({0.f, height})
+          foreground({0.f, height}),
+          font(&FontManager::instance().get("assets/fonts/OpenSans-Italic.ttf")),
+          text(*font)
     {
         background.setFillColor(sf::Color(50, 50, 50, 180));
         background.setOutlineThickness(1.f);
         background.setOutlineColor(sf::Color::Black);
 
         foreground.setFillColor(sf::Color::Green);
+        text.setCharacterSize(14);
+        text.setFillColor(sf::Color::White);
     }
 
     void setProgress(float progress)
@@ -21,26 +26,26 @@ public:
         progress = std::clamp(progress, 0.f, 1.f);
         foreground.setSize({background.getSize().x * progress,
                             background.getSize().y});
+        text.setString(std::to_string(static_cast<int>(progress * 100)) + "%");
     }
 
-    void setPosition(const sf::Vector2f& pos)
+    void setPosition(const sf::Vector2f &pos)
     {
         background.setPosition(pos);
         foreground.setPosition(pos);
+        text.setPosition(pos + sf::Vector2f(5.f, 0.f));
     }
 
-    void draw(sf::RenderTarget& target) const
+    void draw(sf::RenderTarget &target) const
     {
         target.draw(background);
         target.draw(foreground);
+        target.draw(text);
     }
 
 private:
-void printProgressPercentage(float progress)
-{
-    int percentage = static_cast<int>(progress * 100);
-    std::cout << "Progress: " << percentage << "%\n";
-}
     sf::RectangleShape background;
     sf::RectangleShape foreground;
+    const sf::Font* font;
+    sf::Text text;
 };
